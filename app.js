c47b33c45c95aa7e0a895756cb226b4d6b6c968f
Shahzaib siraj <64611800+sbrjprogrammer@users.noreply.github.com>
2021-07-30 11:12:10 -0700

    Update app.js
    ye

--- app.js
+++ app.js
@@ -267,3 +267,31 @@
 // console.log(check_numbers(12, 101));
 // console.log(check_numbers(52, 80));
 // console.log(check_numbers(15, 99));
+
+
+
+// Write a JavaScript program to find a value which is nearest to 100 from two different given integer values.
+
+// function near_100(x, y) {
+//   if (x != y)
+//   {
+//   x1 = Math.abs(x - 100);
+//   y1 = Math.abs(y - 100);
+
+//   if (x1 < y1)
+//   {
+//     return x;
+//   }
+//   if (y1 < x1)
+//   {
+//     return y;
+//   }
+//   return 0;
+//   }
+//   else
+//     return false;
+// }
+
+// console.log(near_100(90, 89));
+// console.log(near_100(-90, -89));
+// // console.log(near_100(90, 90));
